39fbc696358df7cc9b51b066efb3e2fd129fb544
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-10 12:44:15 -0300

    Models for stripe integration.

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Price, Product
+
 # Register your models here.
+
+
+class PriceInline(admin.TabularInline):
+    model = Price
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PriceInline]
+
+
+admin.site.register(Product, ProductAdmin)
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    stripe_price_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Price(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stripe_price_id = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+
+    def get_display_price(self):
+        return "{0:.2f}".format(self.price / 100)
